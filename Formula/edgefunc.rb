8248dc9b6bf80303e74a7cce6acc5df3acedad11
Srikanth Garlapati <gsri@umich.edu>
2021-01-21 17:14:18 -0500

    Update formula

--- Formula/edgefunc.rb
+++ Formula/edgefunc.rb
@@ -1,9 +1,9 @@
 class Edgefunc < Formula
   desc "A Limelight Networks Command Line Interface used to call the EdgeFunctions API"
   homepage "https://support.limelight.com/public/openapi/edgefunctions/index.html#section/Overview"
-  url "https://developers.limelight.com/downloads/edgefunc/prod/edgefunc-current-amd64-macos.zip"
-  sha256 "086f1e98cacb3f6ae91525677c4cfe96c7bd05c8cbfc1f317be3db5d07334bf2"
-  version "1.0.2"
+  url "https://developers.limelight.com/downloads/edgefunc/prod/edgefunc-1.0.3-amd64-macos.zip"
+  sha256 "fbcd0b65c8b67e527cf34617ebcde5abf5b4e2788d6db8b949456a19665d1ec7"
+  version "1.0.3"
 
   def install
     bin.install "edgefunc"
